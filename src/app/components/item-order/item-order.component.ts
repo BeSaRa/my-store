9d688c97d165a28ec2ddcd45ef6ee5b5d8e56ad4
BeSaRa <besara@windowslive.com>
2022-07-19 07:26:20 +0300

    feat: update quantity from input

--- src/app/components/item-order/item-order.component.ts
+++ src/app/components/item-order/item-order.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { OrderItem } from "../../models/order-item";
 import { CartService } from "../../services/cart.service";
+import { NgModel } from "@angular/forms";
 
 @Component({
   selector: 'app-item-order',
@@ -13,11 +14,17 @@ export class ItemOrderComponent {
   @Output()
   deleteItem: EventEmitter<void> = new EventEmitter<void>()
 
-  constructor(private cartService: CartService) { }
+  constructor(private cartService: CartService) {}
 
   removeItem(): void {
     this.cartService.order.removeItem(this.item.product)
     this.deleteItem.next()
   }
 
+  updateQuantity(ngModel: NgModel, value: number | null): void {
+    const quantity = value && value > 0 ? value : 1;
+    this.item.setQuantity(quantity)
+    // in case if user remove the value and come null we have to update viewModel manually
+    !value && ngModel.valueAccessor?.writeValue(quantity)
+  }
 }
